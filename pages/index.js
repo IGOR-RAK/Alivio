024a27171562783c189091948d6606de3efbea46
Igor Rak <igorrakfed@gmail.com>
2022-01-29 15:45:10 +0100

    last sections

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import HomePage from "../components/HomePage"
 import HowItWorks from "../components/HowItWorks"
+import OurProduct from "../components/OurProduct"
+import Story from "../components/Story"
 
 
 export default function Home() {
   return (
     <div className="main">
        <HomePage/>
-       <HowItWorks/>     
+       <HowItWorks/>
+       <Story/> 
+       <OurProduct/>   
     </div>
   )
 }
