ce37884bad67facf6362943afdde522ea7f4f15a
Igor Rak <igorrakfed@gmail.com>
2022-01-29 16:08:59 +0100

    test6

--- components/Story.jsx
+++ components/Story.jsx
@@ -11,7 +11,11 @@ const Story=() => {
     <section className="story">
         <div className="story-container">
             <div className="story-row">
-               
+                <div className="story-container">
+                        <div className="story-title">Lets hear about Kaylas success story</div>
+                        <div className="story-text">See how well Alivio works in a real customers life. </div>
+                        <a className="btn big" href="#">Lets get started</a>
+                </div>
                 <div className="story-video">
                     <Image src={video} width={896} height={516} alt="logo" />   
                 </div>
